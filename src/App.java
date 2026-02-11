0db863bfa951e64bc2a03b90c8f164296a30bf5d
CooperProvins <cooperprovins@gmail.com>
2026-02-11 10:45:26 -0800

    changes

--- src/App.java
+++ src/App.java
@@ -1,32 +1,43 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.concurrent.Flow.Subscriber;
 
-/**
- * Main application for the Data Analysis Mini‑Project.
- *
- * TODO:
- *  - Update the path to your dataset file
- *  - Read the CSV file using Scanner
- *  - Parse each row and extract the correct columns
- *  - Construct Data objects from each row
- *  - Store them in an array
- *  - Write methods to analyze the dataset (min, max, average, filters, etc.)
- *  - Print insights and answer your guiding question
- *  - Add Javadoc comments for any methods you create
- */
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
+        // import file
         File myFile = new File("C:\\Users\\524234\\OneDrive - San Diego Unified School District\\Documents\\GitHub\\Cereal_Data_Analysis\\cereal.csv");
         Scanner myScanner = new Scanner(myFile);
 
+        // set delimeter for using .next()
         myScanner.useDelimiter(",");
 
-        myScanner.nextLine(); // Skip the header row
-        for (int i = 0; i < 20; i++){
-            System.out.println(myScanner.next());
+        // Skip the header row
+        myScanner.nextLine();
+        
+        // while scanner still has another line
+        while (myScanner.hasNextLine()){
+            // set local variables to variables from current line
+            String name = myScanner.next();
+            char manufacturer = myScanner.next().charAt(0);
+            char type = myScanner.next().charAt(0);
+            int calories = myScanner.nextInt();
+            int fiber = myScanner.nextInt();
+            int carbs = myScanner.nextInt();
+            int sugars = myScanner.nextInt();
+            int potassium = myScanner.nextInt();
+
+            // skip vitamins, shelf, weight, cups
+            myScanner.next();
+            myScanner.next();
+            myScanner.next();
+            myScanner.next();
+
+            // set local rat
+            double rating = myScanner.nextDouble();
+
+            Cereal cereal = new Cereal(name, manufacturer, type, calories, fiber, carbs, sugars, potassium, rating);
+            
         }
 
     }
--- src/Cereal.java
+++ src/Cereal.java
@@ -1,3 +1,50 @@
 public class Cereal {
-    
+    private String name;
+    private char manufacturer;
+    private char type;
+    private int calories;
+    private int fiber;
+    private int carbs;
+    private int sugars;
+    private int potassium;
+    private double rating;
+
+    public Cereal(String name, char manufacturer, char type, int calories, int fiber, int carbs, int sugars, int potassium, double rating){
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.calories = calories;
+        this.fiber = fiber;
+        this.carbs = carbs;
+        this.sugars = sugars;
+        this.potassium = potassium;
+        this.rating = rating;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getCalories() {
+        return calories;
+    }
+    public int getCarbs() {
+        return carbs;
+    }
+    public int getFiber() {
+        return fiber;
+    }
+    public char getManufacturer() {
+        return manufacturer;
+    }
+    public int getPotassium() {
+        return potassium;
+    }
+    public double getRating() {
+        return rating;
+    }
+    public int getSugars() {
+        return sugars;
+    }
+    public char getType() {
+        return type;
+    }
 }
